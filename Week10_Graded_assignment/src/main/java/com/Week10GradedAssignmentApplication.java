08cf7ace9f1cc11b368d4239634a90e511e432f2
Your Name <you@example.com>
2022-01-19 17:20:30 +0530

    Assignment10 Solution

---
+++ Week10_Graded_assignment/src/main/java/com/Week10GradedAssignmentApplication.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week10GradedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week10GradedAssignmentApplication.class, args);
+		System.out.println("Hello Dev-Ops");
+	}
+
+}
